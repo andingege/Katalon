fa1d7af32cff2a226ad1603413f7208febe25855
lamfete <agust@dbc.co.id>
2022-10-24 20:55:06 +0700

    initial commit

---
+++ Object Repository/homepage/btnLogin.rs
@@ -0,0 +1,19 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>btnLogin</name>
+   <tag></tag>
+   <elementGuidId>18e7fb12-3c45-4fbf-8bfc-9647ed5adfb1</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>BASIC</key>
+         <value></value>
+      </entry>
+      <entry>
+         <key>CSS</key>
+         <value>#navbarText > ul > li > a</value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>CSS</selectorMethod>
+   <useRalativeImagePath>false</useRalativeImagePath>
+</WebElementEntity>
