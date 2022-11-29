b32f8306c5d0372fd952f3106a5b89b8a46657d6
Andin Gege <marpaungandin@gmail.com>
2022-11-29 21:51:25 +0700

    Web/login Andin

---
+++ Object Repository/OR01 - EmailField.rs
@@ -0,0 +1,23 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>OR01 - EmailField</name>
+   <tag></tag>
+   <elementGuidId>b2a3dd88-52c6-4aa8-8ec2-7fb65ee6cd5e</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>BASIC</key>
+         <value>//*[@id = 'exampleInputEmail1']</value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>BASIC</selectorMethod>
+   <useRalativeImagePath>false</useRalativeImagePath>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>id</name>
+      <type>Main</type>
+      <value>exampleInputEmail1</value>
+      <webElementGuid>10cabb3d-a08b-430a-88bf-0486928ccc73</webElementGuid>
+   </webElementProperties>
+</WebElementEntity>
---
+++ Object Repository/OR04 - PasswordField.rs
@@ -0,0 +1,23 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>OR04 - PasswordField</name>
+   <tag></tag>
+   <elementGuidId>17d6a76b-7870-4185-806c-e86f5bb667c5</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>BASIC</key>
+         <value>//*[@id = 'exampleInputPassword1 ']</value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>BASIC</selectorMethod>
+   <useRalativeImagePath>false</useRalativeImagePath>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>id</name>
+      <type>Main</type>
+      <value>exampleInputPassword1 </value>
+      <webElementGuid>e4269d2e-37fc-40b2-bf61-a7c61b27c0b1</webElementGuid>
+   </webElementProperties>
+</WebElementEntity>
