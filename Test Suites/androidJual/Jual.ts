a62a22210c58c24f3f28cf5b71e29397560ac5ae
Aditia Angga Perdana <aditia.anggaperdana@gmail.com>
2022-12-02 15:46:00 +0700

    add android/jual

---
+++ Test Suites/androidJual/Jual.ts
@@ -0,0 +1,44 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>Jual</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>false</rerunImmediately>
+   <testSuiteGuid>1ed2603b-f49a-41aa-9898-8e06b745cf8e</testSuiteGuid>
+   <testCaseLink>
+      <guid>dbade7d8-aad9-4a91-93c3-e0313a1a7537</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/androidJual/jual/TCJJ001 - Jual Isi field dengan data yang valid</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>81907fbb-c2f3-43a1-b274-d4959ed704ee</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/androidJual/jual/TCJJ002 - Jual Input semua field dengan data kosong</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>db3519a9-6d4a-4970-a1a6-c32f1e3cd390</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/androidJual/jual/TCJJ003 - Jual Input semua field dan hanya field harga produk kosong</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>7727d9f6-a57b-4563-a3b1-b3ba465070d4</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/androidJual/jual/TCJJ004 - Jual Input semua field dan hanya field lokasi produk kosong</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>4a36caec-d4a5-44d9-8b06-8302ca891853</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/androidJual/jual/TCJJ005 - Jual Input semua field dan hanya field foto produk kosong</testCaseId>
+   </testCaseLink>
+</TestSuiteEntity>
