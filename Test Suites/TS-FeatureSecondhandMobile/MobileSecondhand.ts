fc1f594cadfa1d7610eb49e8528beda3b9ca843c
Andin Gege <marpaungandin@gmail.com>
2022-12-10 10:09:40 +0700

    Katalon

---
+++ Test Suites/TS-FeatureSecondhandMobile/MobileSecondhand.ts
@@ -0,0 +1,14 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>MobileSecondhand</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>false</rerunImmediately>
+   <testSuiteGuid>12fb4df9-a736-4890-8c3b-aa3c25a28844</testSuiteGuid>
+</TestSuiteEntity>
